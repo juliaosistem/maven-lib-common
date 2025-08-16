4be6144806275a216fdf8ed8fd5a95a2a5bdaa40
djkali <zigmainflables@gmail.com>
2025-08-16 10:37:57 -0500

    de agrega configuracion para libs aun core dto aun no funciona

--- src/main/java/com/common/lib/infraestructure/services/secundary/CrudSecundaryService.java
+++ src/main/java/com/common/lib/infraestructure/services/secundary/CrudSecundaryService.java
@@ -3,19 +3,35 @@ package com.common.lib.infraestructure.services.secundary;
 import com.common.lib.utils.PlantillaResponse;
 
 /**
- * Interfaz secundaria CRUD alineada con Nest: ID como String.
+ * Interfaz secundaria CRUD genérica
+ * @param <RES> Tipo de respuesta
+ * @param <RQ> Tipo de request
+ * @param <E> Tipo de entidad
+ * @param <I> Tipo de ID
  */
 public interface CrudSecundaryService<RES, RQ, E, I> {
 
-    default PlantillaResponse<RES> all() { return new PlantillaResponse<>(); }
+    default PlantillaResponse<RES> all() { 
+        return new PlantillaResponse<>(); 
+    }
 
-    default PlantillaResponse<RES> byId(I id) { return new PlantillaResponse<>(); }
+    default PlantillaResponse<RES> byId(I id) { 
+        return new PlantillaResponse<>(); 
+    }
 
-    default PlantillaResponse<RES> byIdBusiness(Integer idBusiness) { return new PlantillaResponse<>(); }
+    default PlantillaResponse<RES> byIdBusiness(Integer idBusiness) { 
+        return new PlantillaResponse<>(); 
+    }
 
-    default PlantillaResponse<RES> add(RQ request) { return new PlantillaResponse<>(); }
+    default PlantillaResponse<RES> add(RQ request) { 
+        return new PlantillaResponse<>(); 
+    }
 
-    default PlantillaResponse<RES> update(RQ request) { return new PlantillaResponse<>(); }
+    default PlantillaResponse<RES> update(RQ request) { 
+        return new PlantillaResponse<>(); 
+    }
 
-    default PlantillaResponse<RES> delete(I id) { return new PlantillaResponse<>(); }
+    default PlantillaResponse<RES> delete(I id) { 
+        return new PlantillaResponse<>(); 
+    }
 }
